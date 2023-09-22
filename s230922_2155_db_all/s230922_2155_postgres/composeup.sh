3235dc7cfe0538b858cd1b7f0047b6f0fd931ac6
Nam G VU <namgivu@gmail.com>
2023-09-22 22:16:15 +0700

    postgres 0th

---
+++ s230922_2155_db_all/s230922_2155_postgres/composeup.sh
@@ -0,0 +1,12 @@
+docker-compose down  # MUST have down before up sothat envvar get refreshed
+echo
+docker-compose up -d
+
+echo
+
+sleep 1
+docker-compose logs
+
+echo
+docker exec s230922_2155_postgres_postgres_c_1 bash -c 'echo POSTGRES_PASSWORD=$POSTGRES_PASSWORD'
+docker exec s230922_2155_postgres_postgres_c_1 bash -c 'echo     POSTGRES_USER=$POSTGRES_USER'
