2b91ab6f22e31829a8d61d2b25d2852a130c2397
root <namgivu+inetvps@gmail.com>
2023-09-22 21:47:56 +0700

    dockerrun w/ $c container name

--- s230918_1900_webserver_all/s230918_1933_nginx/hack_index_w_volume_mapping/dockerrun/dockerrun.sh
+++ s230918_1900_webserver_all/s230918_1933_nginx/hack_index_w_volume_mapping/dockerrun/dockerrun.sh
@@ -1,5 +1,18 @@
+c=nginx_hackindex_volmapping_dockerrun_c
+
+docker rm -f $c
+
 docker run \
     -p 488:80 \
     -d \
-    -v ./usr/share/nginx/html:/usr/share/nginx/html
-    nginx
+    -v ./usr/share/nginx/html:/usr/share/nginx/html \
+    --name $c nginx
+
+echo
+
+sleep 1
+docker logs $c
+
+echo
+
+http :488 | grep 'Welcome to nginx!' --color=always
