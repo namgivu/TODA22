aa887f35043a29c82bd028ed34fc7aa75edc37a3
Nam G VU <namgivu@gmail.com>
2023-10-02 22:46:51 +0700

    DONE @ use inanotherfolder context_d

--- s231002_1922_Dockerfile/s231002_2211_dockerfile-assignment-1/composeupbuild.sh
+++ s231002_1922_Dockerfile/s231002_2211_dockerfile-assignment-1/composeupbuild.sh
@@ -1,3 +1,6 @@
-SH=`cd $(dirname ${BASH_SOURCE:-$0}) && pwd`
+SH=`cd $(dirname ${BASH_SOURCE:-$0}) && pwd`  # SH aka SCRIPT_HOME aka containing folder of this script file
 cd $SH
-docker-compose up --build
+
+   build_context=`cd "$SH/../../zref/docker_basic/dockerfile-assignment-1" && pwd` \
+build_dockerfile="$SH/Dockerfile" \
+  docker-compose up --build
