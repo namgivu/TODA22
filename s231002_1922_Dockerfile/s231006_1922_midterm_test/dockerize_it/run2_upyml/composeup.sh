2073d29a01eae570887349edc7b338526851c939
Nam G VU <namgivu@gmail.com>
2023-10-06 21:26:26 +0700

    explain why need build

--- s231002_1922_Dockerfile/s231006_1922_midterm_test/dockerize_it/run2_upyml/composeup.sh
+++ s231002_1922_Dockerfile/s231006_1922_midterm_test/dockerize_it/run2_upyml/composeup.sh
@@ -1,7 +1,7 @@
 SH=`cd $(dirname ${BASH_SOURCE:-$0}) && pwd`
 cd $SH
 
-# docker-compose build  #TODO do we need build before up?
+# docker-compose build  #NOTE we need build before up, why? In reallife project, we run the container on the cloud <- the dokerimage is ready and push onto dockerhub --> need build it first
 echo
 
    context="$SH/../../dockerize_it/context_d--flask_webapp" \
