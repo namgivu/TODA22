cc34eb0fe953cbcda1215373f312b103a315adc9
Nam G VU <namgivu@gmail.com>
2023-10-09 20:11:16 +0700

    run flaskapp

--- s231002_1922_Dockerfile/s231009_1933_composeassignment1/context_d__flaskapp/flask-app/composeup.sh
+++ s231002_1922_Dockerfile/s231009_1933_composeassignment1/context_d__flaskapp/flask-app/composeup.sh
@@ -1,4 +1,6 @@
 SH=`cd $(dirname ${BASH_SOURCE:-$0}) && pwd`
 cd $SH
-  docker-compose build --no-cache
+  # docker-compose build --no-cache  # turn back on when needed the rebuild
+  docker-compose build
+
   docker-compose up
